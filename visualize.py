89514007d436b4ea49923576732ff3ea639e816e
ZHEN_ZI233 <1994577746@qq.com>
2023-04-08 13:53:21 +0800

    修改visualize.py

--- visualize.py
+++ visualize.py
@@ -1,5 +1,5 @@
 # 导入所需的库
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTk
 import tkinter as tk
 
 # 定义一个绘制并标记出篡改区域的函数，接收一个图像对象和一个篡改区域列表作为参数，返回一个新的图像对象
@@ -17,9 +17,11 @@ def draw_regions(img, regions):
 
 # 定义一个在画布上显示图像对象的函数，接收一个画布对象和一个图像对象作为参数，无返回值
 def show_image(canvas, img):
-    # 使用PIL库中的Image类的resize方法，将图像对象缩放为和画布一样大小，并返回一个新的图像对象
-    img = img.resize((canvas.winfo_width(), canvas.winfo_height()))
-    # 使用PIL库中的Image类的tkphoto方法，将图像对象转换为tkinter兼容的图片对象，并返回
-    photo = Image.tkphoto(img)
-    # 使用画布对象的create_image方法，在画布上创建并显示图片对象
-    canvas.create_image(0, 0, image=photo, anchor=tk.NW)
+    # 使用PIL库中的Image类的resize方法，将图像对象按比例缩放来兼容画布，并返回一个新的图像对象
+    scale_factor = float(img.height / canvas.winfo_height())
+    img = img.resize((int(img.width / scale_factor), canvas.winfo_height()))
+    # 使用PIL库中的ImageTk类的PhotoImage类来创建tkinter兼容的图片对象，并返回
+    photo = ImageTk.PhotoImage(img)
+    # 使用画布对象的create_image方法和itemconfig方法，在画布上创建并显示图片对象
+    main = canvas.create_image(0, 0, image=photo, anchor=tk.NW)
+    canvas.itemconfig(main, photo)
